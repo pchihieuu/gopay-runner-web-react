b453de8817705719e3ebb34db24acf3fd93b25d4
pchihieuu <hieup3611@gmail.com>
2023-11-12 00:00:45 +0700

    done project game gopay runner website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
+import {useState, useEffect, useCallback} from 'react'
 
 function App() {
+  const [received, setReceived] = useState(0)
+
+  const { unityProvider, sendMessage, addEventListener, removeEventListener, loadingProgression,isLoaded } = useUnityContext({
+    loaderUrl: "../Build/Build/Build.loader.js",
+    frameworkUrl: "../Build/Build/Build.framework.js",
+    dataUrl: "../Build/Build/Build.data",
+    codeUrl: "../Build/Build/Build.wasm"
+  });
+
+
+  function sendDataToUnity() {
+    sendMessage("GameController", "UpdateMessage", "plop la zone");
+  }
+
+  const handleUnityButtonClicked = useCallback((count) => {
+    console.log("btn clicked", count)
+    setReceived(count)
+  }, []);
+
+
+  
+
+ 
+  useEffect(() => {
+    addEventListener("ButtonClicked", handleUnityButtonClicked);
+    return () => {
+      removeEventListener("ButtonClicked", handleUnityButtonClicked);
+    };
+  }, [addEventListener, removeEventListener, handleUnityButtonClicked]);
+ 
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Unity style={{ width: 1300, height: 650 }} unityProvider={unityProvider} />
+      
+     
     </div>
   );
 }
